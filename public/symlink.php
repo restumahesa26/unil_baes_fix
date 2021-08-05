8fa4cca7756050b672b754aa6146da3bab8bdabf
Mufti Restu Mahesa <mufti.restumahesa@gmail.com>
2021-08-05 13:40:43 +0700

    update symlink

--- public/symlink.php
+++ public/symlink.php
@@ -1,7 +1,7 @@
 <?php
 
 $targetFolder = __DIR__.'/../laravel/storage/app/public';
-$linkFolder = __DIR__.'/storage';
+$linkFolder = __DIR__.'/public/storage/';
 symlink($targetFolder, $linkFolder);
 
 echo 'Symlink process succesfully completed';
